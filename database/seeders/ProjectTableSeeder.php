5175c18b196cbe9926b827f990f956d5e4b14a52
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-19 20:22:40 +0200

    feat: data fake seeder

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
 
 class ProjectTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+            $newProject->title = $faker->sentence(5);
+            $newProject->content = $faker->text(500);
+            $newProject->slug = Str::slug($newProject->title);
+            $newProject->save();
+        }
     }
 }
