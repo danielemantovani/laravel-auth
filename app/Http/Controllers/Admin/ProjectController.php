4df7b7c0349a3a34249abfbed850536f11d7250a
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-19 21:14:28 +0200

    feat: funciotn index controller

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $project= Project::all();
+        return view('admin.index', compact('project'));
     }
 
     /**
